36ed74f2cb469933555f83bbb01af0497e05557d
Aurélio Felipe <afelipelm@gmail.com>
2021-07-24 15:30:29 -0300

    Added task entity information.

--- apps/api/src/app/modules/tasks/entities/task.entity.ts
+++ apps/api/src/app/modules/tasks/entities/task.entity.ts
@@ -1,1 +1,22 @@
-export class Task {}
+import { Model } from "sequelize";
+import { Column, DataType, Table } from "sequelize-typescript";
+import { Status } from "@nt-al/api-interfaces"
+
+@Table({
+  modelName: 'tasks',
+  timestamps: true,
+  paranoid: true,
+  underscored: true
+})
+export class Task extends Model {
+  @Column({
+    primaryKey: true
+  })
+  id?: number;
+
+  @Column(DataType.STRING)
+  status?: Status;
+
+  @Column
+  description: string;
+}
