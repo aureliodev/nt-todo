90695fb7e4105eff629e96d915889870da2c793e
Aurélio Felipe <afelipelm@gmail.com>
2021-08-13 14:59:49 -0300

    Add app routing.

---
+++ apps/front/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'tarefas',
+    loadChildren: () => import('./modules/task/task.module').then(m => m.TaskModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled'
+    })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
